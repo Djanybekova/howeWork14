9395e1b626fb89eaf7f5a86df1655fae3e67cb57
Aiperi <104366856+Djanybekova@users.noreply.github.com>
2022-05-14 17:15:51 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-	Programmer programmer = new Programmer("Sky ","Programmer ","Nasa");
+    Programmer programmer = new Programmer("Sky ","Programmer ","Nasa");
     Dancer dancer = new Dancer("Moon ","Dancer ","Star");
-    Singer singer = new Singer("Sun ","Singer ","Clouds") ;
+    Singer singer = new Singer("Sun ","Singer ","Clouds");
         System.out.println(programmer);
         System.out.println(dancer);
         System.out.println(singer);
